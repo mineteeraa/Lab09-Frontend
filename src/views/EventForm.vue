8e4b5d9dae50d3a24b0bae6ed0b91ad080a77c4f
mineteeraa <patteera_mine@hotmail.com>
2021-09-09 20:11:49 +0700

    [6.6] Create BaseSelect and update EventForm

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -13,17 +13,11 @@
       <BaseInput v-model="event.location" type="text" label="Location" />
 
       <h3>Who is your organizer?</h3>
-      <label>Select an Organizer</label>
-      <select v-model="event.organizer.id">
-        <option
-          v-for="option in GStore.organizers"
-          :value="option.id"
-          :key="option.id"
-          :selected="option.id === event.organizer.id"
-        >
-          {{ option.name }}
-        </option>
-      </select>
+      <BaseSelect
+        :options="GStore.organizers"
+        v-model="event.organizer.id"
+        label="Select an Organizer"
+      />
 
       <button type="submit">Submit</button>
     </form>
